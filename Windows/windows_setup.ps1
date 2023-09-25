908114b9f2b08131515de5f90da4bd07e919c079
Miquest <thorin_eichenschild@mein.gmx>
2023-09-25 18:16:02 +0200

    Edited windows setup script

--- Windows/windows_setup.ps1
+++ Windows/windows_setup.ps1
@@ -1,22 +1,42 @@
 $downloadUrl = "https://github.com/Miquest/ohg_auto_login/archive/main.zip"
 $extractedFolderPath = "ohg_login_windows"
 $savePath = "ohg_auto_download.zip"
+$installDirectory = "C:\scripts\ohg_auto_login\"
 
 Invoke-WebRequest -Uri $downloadUrl -OutFile $savePath
 
-# Überprüfe, ob der Download erfolgreich war
+# Check if the download was successful
 if (Test-Path $savePath) {
-    Write-Host "Download erfolgreich! Die Zip-Datei wurde unter $savePath gespeichert."
+    Write-Host "Download der Programmdateien erfolgreich!"
 
-    # Entpacke die Zip-Datei in den angegebenen Ordner
+    # Unzip the .zip archive folder
     Expand-Archive -Path $savePath -DestinationPath $extractedFolderPath
 
-    # Überprüfe, ob die Entpackung erfolgreich war
+    # Check if the files were extracted 
     if (Test-Path $extractedFolderPath) {
-        Write-Host "Entpacken erfolgreich! Die Dateien wurden unter $extractedFolderPath gespeichert."
+        Write-Host "Entpacken der Dateien erfolgreich! Installation wird gestartet"
+
+        Write-Information "Die Dateien werden in den Dokumentenordner entpackt!"
+        Remove-Item -Path "ohg_login_windows/ohg_auto_login-main/Linux" -Recurse
+
+        mkdir $installDirectory
+
+        if (Test-Path $installDirectory){
+            Write-Debug "Installationsordner wurde erstellt! Verschiebe die Dateien in installationsordner..."
+
+            Copy-Item -Path "ohg_login_windows/ohg_auto_login-main/Windows/*" -Destination $installDirectory -Recurse
+            # Remove installation Script from installation folder
+            Remove-Item -Path "C:\scripts\ohg_auto_login\windows_setup.ps1"
+
+        } else {
+            Write-Error "Erstellen des Installationsverzeichnisses fehlgeschlagen!"
+        }
+
     } else {
         Write-Host "Entpacken fehlgeschlagen."
     }
 } else {
     Write-Host "Download fehlgeschlagen."
 }
+
+Write-Host "Programm wurde in 'C:\scripts\ohg_auto_login' installiert. Zum Starten, bitte die Datei 'ohg-script-daemon.pyw' ausführen!"
