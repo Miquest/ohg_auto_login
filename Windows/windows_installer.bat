b41481ba2ebe1d8fd7a9f7a1264d5cc9ffaa024b
Miquest <91971937+Miquest@users.noreply.github.com>
2023-09-26 19:45:07 +0200

    Added new Windows installer!

---
+++ Windows/windows_installer.bat
@@ -0,0 +1,68 @@
+@echo off
+:: Überprüfen, ob das Skript mit Administratorrechten ausgeführt wird
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" || (
+    echo Dieses Skript erfordert Administratorrechte. Bitte starte das Skript mit Administratorrechten!
+    pause
+    goto :eof
+)
+
+setlocal
+
+set "downloadUrl=https://github.com/Miquest/ohg_auto_login/archive/main.zip"
+set "extractedFolderPath=ohg_login_windows"
+set "savePath=ohg_auto_download.zip"
+set "installDirectory=C:\scripts\ohg_auto_login"
+set "startupDirectory=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+set "programExecutable=ohg-script-daemon.exe"
+
+echo Programmdateien werden heruntergeladen...
+
+powershell -command "Invoke-WebRequest -Uri '%downloadUrl%' -OutFile '%savePath%'"
+
+:: Überprüfen, ob der Download erfolgreich war
+if exist "%savePath%" (
+    echo Download der Programmdateien erfolgreich!
+    
+    :: Entzippen des ZIP-Archivs
+    powershell -command "Expand-Archive -Path '%savePath%' -DestinationPath '%extractedFolderPath%'"
+    
+    :: Überprüfen, ob die Dateien entpackt wurden
+    if exist "%extractedFolderPath%" (
+
+        echo Entpacken der Dateien erfolgreich! Installation wird gestartet.
+        
+        echo Die Dateien werden in den Installationsordner verschoben...
+        rmdir /s /q "ohg_login_windows\ohg_auto_login-main\Linux"
+        
+        mkdir "%installDirectory%"
+        
+        :: Überprüfen, ob das Installationsverzeichnis erstellt wurde
+        if exist "%installDirectory%" (
+            echo Installationsordner wurde erstellt! Verschiebe die Dateien in das Installationsverzeichnis...
+            
+            xcopy /s /e /i "ohg_login_windows\ohg_auto_login-main\Windows\Package\*" "%installDirectory%"
+            
+            :: Kopieren der Verknüpfung zum Programm in den Startup-Ordner
+	    xcopy /s /e /i "%installDirectory%\ohg_auto_login_startup.lnk" "%HOMEDRIVE%%HOMEPATH%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+
+        ) else (
+            echo Erstellen des Installationsverzeichnisses fehlgeschlagen!
+        )
+    ) else (
+        echo Entpacken fehlgeschlagen.
+    )
+) else (
+    echo Download fehlgeschlagen.
+)
+
+:: Entfernen der temporären Dateien und Ordner
+rmdir /s /q "ohg_login_windows"
+del "%savePath%"
+
+echo Das Programm wurde in 'C:\scripts\ohg_auto_login' installiert und zur automatischen Ausführung im Startup-Ordner hinzugefügt.
+echo Zum Starten, bitte das Programm "login-script-daemon.exe" im Installationsverzeichnis ("C:\scripts\ohg_auto_login") ausfuehren.
+echo Vergiss nicht, die config.json im Installationsverzeichnis zu bearbeiten, damit die Login Prozedur ablaeuft wie gewuenscht.
+echo Bei Fehlern, schau dir die .log Datei im Installationsverzeichnis an.
+echo Drücke die Enter-Taste, um fortzufahren.
+pause
