0f3aff500a031481fd12fc60f9b7dda78c18ac56
Miquest <91971937+Miquest@users.noreply.github.com>
2023-10-05 20:14:45 +0200

    Update linux_setup.sh

--- Linux/linux_setup.sh
+++ Linux/linux_setup.sh
@@ -1,3 +1,26 @@
+#!/bin/bash
+
+# Debian-Paket-Repository zur sources.list hinzufügen
+echo "Füge Debian-Paket-Repository zur sources.list hinzu..."
+echo "deb http://deb.debian.org/debian/ buster main contrib non-free" >> /etc/apt/sources.list
+
+# Aktualisiere die Paketlisten
+echo "Aktualisiere die Paketlisten..."
+apt update
+
+# Lösche das Debian-Paket-Repository aus der sources.list
+echo "Lösche das Debian-Paket-Repository aus der sources.list..."
+sed -i '/deb http:\/\/deb.debian.org\/debian\/ buster main contrib non-free/d' /etc/apt/sources.list
+
+# Aktualisiere erneut, um das Repository zu deaktivieren
+echo "Aktualisiere die Paketlisten erneut..."
+apt update
+
+echo "Das Debian-Paket-Repository wurde erfolgreich zur sources.list hinzugefügt und anschließend entfernt. Die Paketlisten wurden aktualisiert."
+
+# Optional: Du kannst hier weitere Befehle oder Anpassungen hinzufügen, je nach Bedarf.
+
+
 # Setup script for my school project
 echo "Starting initiating the setup process for the program"
 
